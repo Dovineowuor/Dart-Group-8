e83c432b7319f0f6abf7f2a16f9d12c277e204b8
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-26 22:38:26 +0300

    Update dashboard.dart

--- backend/dashboard.dart
+++ backend/dashboard.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+// Dashboard function 
+// This function declares a dashboard
+// This file only renders each widget only wheen the user is authenticated 
+
+// Dashboard
 class Dashboard extends StatefulWidget {
   @override
   _DashboardState createState() => _DashboardState();
 }
-
+// Check account authentication status
 class _DashboardState extends State<Dashboard> {
   Future<String> getAccountStatus() async {
     var response = await http.get('http://example.com/account/status');
@@ -15,7 +20,7 @@ class _DashboardState extends State<Dashboard> {
       return 'Authentication failed';
     }
   }
-
+// Check account ballance only if authenticated
   Future<String> getBalance() async {
     var response = await http.get('http://example.com/account/balance');
     if (response.statusCode == 200) {
@@ -24,7 +29,7 @@ class _DashboardState extends State<Dashboard> {
       return 'Authentication failed';
     }
   }
-
+//  Check and return full account statement only if authenticated to
   Future<String> getFullStatement() async {
     var response = await http.get('http://example.com/account/full-statement');
     if (response.statusCode == 200) {
@@ -33,7 +38,7 @@ class _DashboardState extends State<Dashboard> {
       return 'Authentication failed';
     }
   }
-
+// Acquiring mini-statement only if authenticated
   Future<String> getMiniStatement() async {
     var response = await http.get('http://example.com/account/mini-statement');
     if (response.statusCode == 200) {
@@ -42,7 +47,7 @@ class _DashboardState extends State<Dashboard> {
       return 'Authentication failed';
     }
   }
-
+// Display account transactions analytics only if authenticated
   Future<String> getAnalytics() async {
     var response = await http.get('http://example.com/account/analytics');
     if (response.statusCode == 200) {
@@ -51,7 +56,7 @@ class _DashboardState extends State<Dashboard> {
       return 'Authentication failed';
     }
   }
-
+// Render dashboard properties
   @override
   Widget build(BuildContext context) {
     return Scaffold(
